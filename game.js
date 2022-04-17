5470de04c08e368960c017f98eff318ededb13cd
pavel.uxd <44015330+paveluxd@users.noreply.github.com>
2022-04-17 12:51:10 +0200

    1

--- game.js
+++ game.js
@@ -18,7 +18,7 @@
     };
 
     var game = new Phaser.Game(config)
-    var player, cursors, enemy, fireball, expNode, staff
+    var player, cursors, enemy, fireball, expNode, spike, scene
 
     //Player stats
     var playerStats = {
@@ -26,24 +26,38 @@
         speed: 200,
         points: 0,
         experiece: 0,
-        level: 0,
+        requiredExp: 10,
+        level: 1,
         weapons:{
             fireball: {
                 equiped: true,
                 direction: 'top', 
                 damage: 5,
-                castSpeed: 1600,
+                castSpeed: 2000,
                 projSpeed: 360,
                 dpsTimeout: 500, //how frequently projectile deals damage
+                fadesOnHit: true, //projectiled destroyed on collision
+                lastCastTime: Date.now()
+            },
+            spike: {
+                equiped: true,
+                damage: 7,
+                castSpeed: 5000,
+                projSpeed: 0,
+                dpsTimeout: 500, //how frequently projectile deals damage
+                fadesOnHit: true, //projectiled destroyed on collision
+                lastCastTime: Date.now()
             },
             staff: {
                 equiped: true,
                 direction: 'left', 
                 damage: 10,
-                castSpeed: 1600,
+                castSpeed: 1500,
                 projSpeed: 0,
                 dpsTimeout: 500, //how frequently projectile deals damage
-            }
+                fadesOnHit: false, //projectiled destroyed on collision
+                lastCastTime: Date.now()
+            },
         }
     }
 
@@ -51,10 +65,13 @@
         speed: 80
     }
 
+
 //PRELOAD
     function preload (){
         //Player
         this.load.image('player', './img/player.png')
+        this.load.image('player-hit', './img/player-hit.png')
+        this.load.spritesheet('player-walk', './img/player-walk.png', { frameWidth: 52, frameHeight: 68 });
 
         //Enemy
         this.load.image('enemy-1', './img/enemy-1.png')
@@ -66,11 +83,15 @@
 
         //Projectiles
         this.load.image('fireball', './img/fireball.png')
+        this.load.image('spike', './img/spike.png')
         this.load.image('staff', './img/staff.png')
     }
 
 //CREATE
     function create (){
+        // this.scene.pause()
+        scene = this.scene
+
         //Variables for screen centre
         const screenCenterX = this.cameras.main.worldView.x + this.cameras.main.width / 2;
         const screenCenterY = this.cameras.main.worldView.y + this.cameras.main.height / 2;
@@ -82,15 +103,20 @@
         player = this.physics.add.sprite(screenCenterX, screenCenterY, 'player')
         player.setCollideWorldBounds(true);
 
+        this.anims.create({
+            key: 'walk',
+            frames: this.anims.generateFrameNumbers('player-walk', { start: 0, end: 2 }),
+            frameRate: 2,
+            repeat: -1
+        });
+
+        player.anims.play('walk', true);
+
+
         //Enemy sprite
         enemy = this.physics.add.group();
-        
         setInterval(function()
         {
-            let screenX = config.width
-            let screenY = config.height
-            let unitSize = 80
-
             if(Math.floor(Math.random()* 10) > 5){
                 enemy.create(
                     (Phaser.Math.Between(-80,-600)),
@@ -113,10 +139,17 @@
             lastEnemy.lastHitTime = Date.now()
         }, 2000)
 
-
         //Drop exp node sprite
         var expText = document.getElementById('exp')
         expText.innerHTML = playerStats.experiece
+
+        // Shows Required EXP
+        var nextLevelText = document.getElementById('nextlvl')
+        nextLevelText.innerHTML = "Next Level at : " + playerStats.requiredExp
+
+        // Shows Player Level
+        var lvlText = document.getElementById('level')
+        lvlText.innerHTML = "Level :" + playerStats.level
 
         expNode = this.physics.add.group()
         this.physics.add.overlap(player, expNode, collectExp, null, this);
@@ -124,46 +157,82 @@
             playerStats.experiece += 1
             expText.innerHTML = playerStats.experiece
             expNode.destroy()
+
+            // Checks if playerEXP meets Required EXP for level UP
+            if(playerStats.experiece == playerStats.requiredExp)
+            {
+                playerStats.requiredExp += playerStats.level * 10 *(playerStats.level + 1)
+                playerStats.level++
+                nextLevelText.innerHTML = "Next Level at : " +playerStats.requiredExp
+                lvlText.innerHTML = "Level :" + playerStats.level
+                scene.pause();// Pauses game
+                hide(); // pops up Modal
+            }
         }
 
         //WEAPONS
-            //Fireball
+            //Group cretion
             fireball = this.physics.add.group()
-
-            setInterval(function(){
-                if(playerStats.weapons.fireball.equiped === true){
-                    fireball.create(player.x, player.y,'fireball')
-                    //Hit only top
-                    if (playerStats.weapons.fireball.direction === 'top'){
-                    fireball.children.entries.forEach(function (elem){                 
-                        elem.x = player.x
-                        elem.y = player.y  
-                        elem.angle = 90
-
-                        elem.setVelocityY(-Math.abs(playerStats.weapons.fireball.projSpeed))
-                        elem.setVelocityX(0)
-                    })
-                }
-                }
-            }, playerStats.weapons.fireball.castSpeed);//repeat attack
-
-            //Staff
+            spike = this.physics.add.group()
             staff = this.physics.add.group()
 
             setInterval(function(){
-                if(playerStats.weapons.staff.equiped === true){
-                    //Hit only top
-                    if (playerStats.weapons.staff.direction === 'left'){
-                        staff.create(player.x, player.y,'staff')
-                    }
+                //Fireball
+                console.log()
+                if(
+                    playerStats.weapons.fireball.equiped === true && 
+                    Date.now() - playerStats.weapons.fireball.lastCastTime > playerStats.weapons.fireball.castSpeed
+                ){
+                    //Update cast time
+                    playerStats.weapons.fireball.lastCastTime = Date.now()
+
+                    fireball.create(player.x, player.y,'fireball')
+                    
+                    fireball.children.entries.forEach(function (elem){                 
+                        elem.angle = 90
+                        elem.setVelocityY(-Math.abs(playerStats.weapons.fireball.projSpeed))
+                    })
                 }
-            }, playerStats.weapons.staff.castSpeed);//repeat attack
+
+                //Spike
+                if (
+                    playerStats.weapons.spike.equiped === true && 
+                    Date.now() - playerStats.weapons.spike.lastCastTime > playerStats.weapons.spike.castSpeed
+                ){
+                    //Update cast time
+                    playerStats.weapons.spike.lastCastTime = Date.now()
+
+                    //Drop spike
+                    spike.create(player.x + rNum(80), player.y + rNum(80), 'spike')
+                }
+
+                //Staff
+                if (
+                    playerStats.weapons.staff.equiped === true && 
+                    Date.now() - playerStats.weapons.staff.lastCastTime > playerStats.weapons.staff.castSpeed
+                ){
+
+                    //Update cast time
+                    playerStats.weapons.staff.lastCastTime = Date.now()
+                
+                    staff.create(player.x - 40, player.y, 'staff')
+                    player.setTexture('player-hit')
+
+                    setTimeout(function(){
+                        staff.children.entries.forEach(function(elem){elem.destroy()})
+                        player.setTexture('player')
+
+                    }, 200)
+                }
+            }, 200);//repeat attack
         
 
         //Colliders
         this.physics.add.overlap(player, enemy, dmgPlayer, null, this)  //Player vs enemy
         this.physics.add.overlap(fireball, enemy, dmgEnemy, null, this) //Fireball vs enemy
+        this.physics.add.overlap(spike, enemy, dmgEnemy, null, this) //Spike vs enemy
         this.physics.add.overlap(staff, enemy, dmgEnemy, null, this) //Staff vs enemy
+
 
         this.physics.add.collider(enemy, enemy) //Enemy vs Enemy
 
@@ -185,7 +254,6 @@
         
         //Player movement wihth keyboard
         if (cursors.left.isDown) {
-            // player.x -= 3
             player.setVelocityX(-Math.abs(playerStats.speed))
 
             //Turn drift
@@ -255,6 +323,13 @@
                 element.setVelocityY(0)
             }
         })
+
+        //Projectile movement
+        staff.children.entries.forEach(function(elem){
+            elem.x = player.x - 40
+            elem.y = player.y
+        })
+        
     }
 
 //GAME LOGIC
@@ -276,7 +351,7 @@
             //Reduce live
             playerStats.life--
 
-            //Check if player is dead
+            //Player dead
             if(playerStats.life < 1){
 
                 //Reset player object
@@ -299,39 +374,31 @@
 
     
     //ENEMY DAMAGE
-    function dmgEnemy(weaponObj, enemy){
-
-        var weapon = weaponObj.texture.key
+    function dmgEnemy(projectile, enemy){
         var enemyHp = enemy.customHp
-        var weaponDmg = playerStats.weapons[weapon].damage
+        var weaponDmg = playerStats.weapons[projectile.texture.key].damage
 
         //Enemy dead
-        if (Date.now() - enemy.lastHitTime > playerStats.weapons[weapon].dpsTimeout){
+        if (Date.now() - enemy.lastHitTime > playerStats.weapons[projectile.texture.key].dpsTimeout){
+            //Update hit timer to prevent hit stacking
             enemy.lastHitTime = Date.now()
 
+            //Enemy killed
             if(enemyHp <= weaponDmg){
                 //Drop exp node
                 expNode.create(enemy.x, enemy.y, 'exp-node')
+
                 //Destory enemy
                 enemy.destroy()
-                
             }
             //Enemy hit
             else{
                 enemy.customHp -= weaponDmg
                 enemy.setTexture('enemy-1-hit') //changes image when hit
-                
+            }
 
-                if(weapon === 'staff'){
-                    staff.children.entries.forEach(function (elem){                     
-                        elem.destroy()
-                    })
-                } 
-                else {
-                    fireball.children.entries.forEach(function (elem){                     
-                        elem.destroy()
-                    })
-                }
+            if(playerStats.weapons[projectile.texture.key].fadesOnHit === true){
+                projectile.destroy()
             }
         }
     }
@@ -340,4 +407,13 @@
     function hide(){
         var modal = document.getElementById('modal')
         modal.classList.toggle('hide')
+    }
+
+    function rNum(maxValue){
+        var value = Math.floor(Math.random() * maxValue)
+        return value
+    }
+
+    function resumeScene(){
+        scene.resume();
     }
